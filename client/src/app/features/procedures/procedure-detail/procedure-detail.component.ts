14f41ab7d169709e120a8357b0341e873d46f977
Fernando Siller <fersill53@gmail.com>
2025-11-24 23:06:03 -0600

    added a delete option

--- client/src/app/features/procedures/procedure-detail/procedure-detail.component.ts
+++ client/src/app/features/procedures/procedure-detail/procedure-detail.component.ts
@@ -58,7 +58,7 @@ export class ProcedureDetailComponent {
     this.editable.instruments?.splice(i, 1);
   }
 }
-*/
+*
 
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
@@ -124,5 +124,85 @@ export class ProcedureDetailComponent {
   removeInstrument(i: number) {
     if (!this.editable) return;
     this.editable.instruments?.splice(i, 1);
+  }
+}
+*/
+
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule, ActivatedRoute, Router } from "@angular/router";
+import { ProceduresService } from "../../../core/services/procedures.service";
+import { Procedure } from "../../../core/models/procedure.model";
+import { switchMap } from "rxjs/operators";
+import { Observable } from "rxjs";
+import { FormsModule } from "@angular/forms";
+
+@Component({
+  selector: 'app-procedure-detail',
+  standalone: true,
+  imports: [CommonModule, RouterModule, FormsModule],
+  templateUrl: './procedure-detail.component.html',
+  styleUrls: ['./procedure-detail.component.scss']
+})
+export class ProcedureDetailComponent {
+  readonly procedure$!: Observable<Procedure | undefined>;
+  editMode = false;
+  editable?: Procedure;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private svc: ProceduresService
+  ) {
+    this.procedure$ = this.route.paramMap.pipe(
+      switchMap(params => {
+        const id = params.get('id')!;
+        return this.svc.getById(id);
+      })
+    );
+  }
+
+  // -------- EDITING --------
+  startEdit(p: Procedure) {
+    this.editable = JSON.parse(JSON.stringify(p)) as Procedure;
+    this.editMode = true;
+  }
+
+  cancelEdit() {
+    this.editMode = false;
+    this.editable = undefined;
+  }
+
+  saveEdit() {
+    if (!this.editable) return;
+    this.svc.updateProcedure(this.editable).subscribe(() => {
+      this.editMode = false;
+      this.editable = undefined;
+      this.router.navigate(['/procedures']);
+    });
+  }
+
+  // -------- DELETE --------
+  confirmDelete(p: Procedure) {
+    const id = p.id ?? (p as any)._id;
+
+    if (!id) {
+      alert("Cannot delete: missing id.");
+      return;
+    }
+
+    const ok = confirm(`Delete procedure "${p.name}"?`);
+    if (!ok) return;
+
+    this.svc.deleteProcedure(id).subscribe({
+      next: () => {
+        alert("Deleted.");
+        this.router.navigate(['/procedures']);
+      },
+      error: err => {
+        console.error("Delete failed", err);
+        alert("Error deleting procedure.");
+      }
+    });
   }
 }
