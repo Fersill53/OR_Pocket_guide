14c2bb9ec148bb9e1fa993c55ab8986abcd5fa6a
Fernando Siller <fersill53@gmail.com>
2025-12-16 22:37:02 -0600

    styling errors

--- client/src/app/features/home/home.component.ts
+++ client/src/app/features/home/home.component.ts
@@ -26,6 +26,8 @@ import { RouterLink } from '@angular/router';
 export class HomeComponent {}
 */
 
+
+/*
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../core/services/auth.service';
@@ -43,5 +45,26 @@ export class HomeComponent {
   logout() {
     this.auth.logout();
     this.router.navigateByUrl('/login');
+  }
+}
+*/
+
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+  constructor(private auth: AuthService) {}
+
+  logout() {
+    this.auth.logout();
+    window.location.href = '/login';
   }
 }
