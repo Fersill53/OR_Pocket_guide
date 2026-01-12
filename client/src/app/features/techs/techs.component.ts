d1143258393cde9b5b6f0d008e153721654cdd71
Fernando Siller <fersill53@gmail.com>
2026-01-11 18:05:58 -0600

    upated the techs portion

--- client/src/app/features/techs/techs.component.ts
+++ client/src/app/features/techs/techs.component.ts
@@ -1,3 +1,4 @@
+/*
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -19,5 +20,27 @@ export class TechsComponent {
 
   isActive(tab: TechsTab): boolean {
     return this.activeTab === tab;
+  }
+}
+*/
+
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+type TechsTab = 'education' | 'backtable' | 'mayo';
+
+@Component({
+  selector: 'app-techs',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './techs.component.html',
+  styleUrls: ['./techs.component.scss']
+})
+export class TechsComponent {
+  activeTab: TechsTab = 'education';
+
+  setTab(tab: TechsTab) {
+    this.activeTab = tab;
   }
 }
